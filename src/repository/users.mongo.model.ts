c4e8cc2cd2b72cbeff347a3e8f33d39782c3685e
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-13 18:15:24 +0100

    Add user model

---
+++ src/repository/users.mongo.model.ts
@@ -0,0 +1,37 @@
+import { model, Schema } from 'mongoose';
+import { User } from '../entities/user';
+
+const userSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+  },
+  passwd: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  addFoods: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Food',
+    },
+  ],
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject._id;
+    delete returnedObject.passwd;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');
