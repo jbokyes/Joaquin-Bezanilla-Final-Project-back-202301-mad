aed9570a15091f0111a65d41712e82ee35e161da
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-13 21:28:04 +0100

    Add login to users controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,6 +2,7 @@ import createDebug from 'debug';
 import { NextFunction, Response, Request } from 'express';
 import { User } from '../entities/user';
 import { HTTPError } from '../error/error.js';
+import { Auth, PayloadToken } from '../helpers/auth';
 import { Repo } from '../repository/repo.interface';
 
 const debug = createDebug('Latino:users-controller');
@@ -23,9 +24,17 @@ export class UsersController {
       });
       if (!data.length)
         throw new HTTPError(403, 'Unauthorized', 'Email not found');
+      if (!(await Auth.compare(req.body.passwd, data[0].passwd)))
+        throw new HTTPError(403, 'Unauthorized', 'Password not found');
+      const payload: PayloadToken = {
+        id: data[0].id,
+        email: req.body.email,
+        role: 'admin',
+      };
+      const token = Auth.createJWT(payload);
       resp.json({
         results: {
-          data,
+          token,
         },
       });
       debug('Login done by: ' + email);
