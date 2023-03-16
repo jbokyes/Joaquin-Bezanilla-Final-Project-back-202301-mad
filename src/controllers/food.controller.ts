329cabadbda663fdca66cc6549d4118ff62da012
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-03-16 16:10:47 +0100

    Add food router and controller

---
+++ src/controllers/food.controller.ts
@@ -0,0 +1,26 @@
+import { Repo } from '../repository/repo.interface';
+import createDebug from 'debug';
+import { NextFunction, Request, Response } from 'express';
+import { Food } from '../entities/food';
+
+const debug = createDebug('latino-foods:food-controller');
+
+export class FoodsController {
+  constructor(public foodRepo: Repo<Food>) {
+    this.foodRepo = foodRepo;
+  }
+
+  async post(req: Request, resp: Response, next: NextFunction) {
+    try {
+      debug('post-method');
+      const newFood = req.body;
+      const data = await this.foodRepo.create(newFood);
+      resp.status(201);
+      resp.json({
+        results: [data],
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
